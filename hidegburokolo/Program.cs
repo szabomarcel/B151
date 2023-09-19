f18bf483bb29872543d75e2ba273523c942fb44d
szabomarcel <szabomarcell34@gmail.com>
2023-09-19 07:58:54 +0200

    feladat

--- hidegburokolo/Program.cs
+++ hidegburokolo/Program.cs
@@ -11,6 +11,16 @@ namespace hidegburokolo
     {
         static void Main(string[] args)
         {
+            Console.Write("Írja be a szoba széleségét: ");
+            int szam1 = int.Parse(Console.ReadLine());
+            Console.Write("Írja be a szoba magassága: ");
+            byte szam2 = int.Parse(Console.ReadLine());
+            Console.Write("Írja be a szoba hosszúságát: ");
+            int szam3 = int.Parse(Console.ReadLine());
+            Console.Write("Írja be a szoba felületét: ");
+            int szam4 = int.Parse(Console.ReadLine());
+            Console.WriteLine(szam1 + szam3 / szam2 * szam4);
+            Console.ReadKey();
         }
     }
 }
